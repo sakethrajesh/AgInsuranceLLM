ecd76a1f79b9565d0f668789af22878d453590d9
vinhantruong <v.truong1478@gmail.com>
2024-02-13 11:07:51 -0500

    change recommended prompts

--- chatbot/components/empty-screen.tsx
+++ chatbot/components/empty-screen.tsx
@@ -6,16 +6,16 @@ import { IconArrowRight } from '@/components/ui/icons'
 
 const exampleMessages = [
   {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
+    heading: 'See if you are eligible for insurance',
+    message: `Am I qualified if I am a not a US citizen "?`
   },
   {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
+    heading: 'Find requirements for your person type',
+    message: 'What do I need as an LLC? \n'
   },
   {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
+    heading: 'Look up definitions for terms',
+    message: `What does final area revenue mean? \n`
   }
 ]
 
@@ -24,15 +24,11 @@ export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
     <div className="mx-auto max-w-2xl px-4">
       <div className="rounded-lg border bg-background p-8">
         <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          Agricultural Insurance LLM Project
         </h1>
         <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
+          This is an interactive conversational assistant to improve
+          agricultural insurance selection, with large language models (LLMs).
         </p>
         <p className="leading-normal text-muted-foreground">
           You can start a conversation here or try the following examples:
