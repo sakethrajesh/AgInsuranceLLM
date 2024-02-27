514a1c25c82f7fd8ce88525ad1f98ed480a5e326
Kyle Hilgenberg <kjhilgen@comcast.net>
2024-02-27 01:10:29 -0500

    better google icon

--- chatbot/components/ui/icons.tsx
+++ chatbot/components/ui/icons.tsx
@@ -139,29 +139,7 @@ function IconGitHub({ className, ...props }: React.ComponentProps<'svg'>) {
 
 function IconGoogle({ className, ...props }: React.ComponentProps<'svg'>) {
   return (
-    <svg
-      role="img"
-      viewBox="0 0 24 24"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="url(#rainbowGradient)"
-      className={cn('size-4', className)}
-      {...props}
-    >
-      <defs>
-        <linearGradient id="rainbowGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-          <stop offset="0%" stopColor="#ff0000" />
-          <stop offset="16.67%" stopColor="#ff8000" />
-          <stop offset="33.33%" stopColor="#ffff00" />
-          <stop offset="50%" stopColor="#00ff00" />
-          <stop offset="66.67%" stopColor="#0000ff" />
-          <stop offset="83.33%" stopColor="#8000ff" />
-          <stop offset="100%" stopColor="#ff00ff" />
-        </linearGradient>
-      </defs>
-      <title>Google</title>
-      {/* Add the path for the Google icon */}
-      <path d="M 16 7 C 14 3 10 3 10 3 C 2 3 2 9 2 11 L 2 13 C 2 15 2 21 10 21 C 18 21 18 15 18 13 L 10 13 L 10 11 L 19 11 C 20 11.6667 20 12.3333 20 13 C 20 15 20 23 10 23 C 0 23 0 15 0 13 L 0 11 C 0 9 0 1 10 1 C 10 1 16 1 18 7"/>
-    </svg>
+<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z" fill="#4285F4"/><path d="M12 23c2.97 0 5.46-.98 7.28-2.66l-3.57-2.77c-.98.66-2.23 1.06-3.71 1.06-2.86 0-5.29-1.93-6.16-4.53H2.18v2.84C3.99 20.53 7.7 23 12 23z" fill="#34A853"/><path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" fill="#FBBC05"/><path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/><path d="M1 1h22v22H1z" fill="none"/></svg>
   )
 }
 
